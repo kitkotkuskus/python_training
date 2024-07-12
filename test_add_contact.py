7ed340ed3a85c8e92d0d5d59d23d3b006910b46f
Dasha <dashaqwa@mail.ru>
2024-07-12 20:36:21 +0300

    Revert "Созданы пакеты для разных зон ответственности"
    Объединила в одну фикстуру группы и контакты

--- test_add_contact.py
+++ test_add_contact.py
@@ -1,28 +1,28 @@
 # -*- coding: utf-8 -*-
 import pytest
 from contact import Contact
-from application_contact import ApplicationContact
+from application import Application
 
 
 @pytest.fixture
-def app_contact(request):
-    fixture = ApplicationContact()
+def app(request):
+    fixture = Application()
     request.addfinalizer(fixture.destroy)
     return fixture
 
 
-def test_add_contact(app_contact):
-    app_contact.login(username="admin", password="secret")
-    app_contact.create_contact(Contact(firstname="Ivan", middlename="Ivanovich", lastname="Ivanov", nickname="IvAnOv",
+def test_add_contact(app):
+    app.login(username="admin", password="secret")
+    app.create_contact(Contact(firstname="Ivan", middlename="Ivanovich", lastname="Ivanov", nickname="IvAnOv",
                                           file_place="C:\\field_image_muholovka.jpg", title="employee", company="bank",
                                           address="Russia, Moscow, Green street 5",home="564585", mobile="89746521278",
                                           work="-", fax="-", email="test@test.ru", email2="-", email3="-", homepage="www.ivanov.ru",
-                                          byear="1990", ayear="2000" ))
-    app_contact.logout()
+                                          byear="1990", ayear="2000"))
+    app.logout()
 
 
-def test_add_empty_contact(app_contact):
-    app_contact.login(username="admin", password="secret")
-    app_contact.create_contact(Contact(firstname="", middlename="", lastname="", nickname="", file_place="", title="", company="", address="",
+def test_add_empty_contact(app):
+    app.login(username="admin", password="secret")
+    app.create_contact(Contact(firstname="", middlename="", lastname="", nickname="", file_place="", title="", company="", address="",
                                           home="", mobile="", work="", fax="", email="", email2="", email3="", homepage="", byear="", ayear=""))
-    app_contact.logout()
+    app.logout()
