b800e05d049713c7a2dd5fba20aa64a33f1973aa
Dasha <dashaqwa@mail.ru>
2024-07-12 22:55:09 +0300

    Revert "Созданы пакеты для разных зон ответственности"
    Выделен класс для управления сессиями

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -12,17 +12,17 @@ def app(request):
 
 
 def test_add_contact(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_contact(Contact(firstname="Ivan", middlename="Ivanovich", lastname="Ivanov", nickname="IvAnOv",
                                           file_place="C:\\field_image_muholovka.jpg", title="employee", company="bank",
                                           address="Russia, Moscow, Green street 5",home="564585", mobile="89746521278",
                                           work="-", fax="-", email="test@test.ru", email2="-", email3="-", homepage="www.ivanov.ru",
                                           byear="1990", ayear="2000"))
-    app.logout()
+    app.session.logout()
 
 
 def test_add_empty_contact(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_contact(Contact(firstname="", middlename="", lastname="", nickname="", file_place="", title="", company="", address="",
                                           home="", mobile="", work="", fax="", email="", email2="", email3="", homepage="", byear="", ayear=""))
-    app.logout()
+    app.session.logout()
