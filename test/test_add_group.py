a7d39c79427b5b30b741b992efd8ee417fbd27d5
Dasha <dashaqwa@mail.ru>
2024-07-22 22:48:06 +0300

    Добавлены проверки на длину списка групп

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -3,9 +3,13 @@ from python_training.model.group import Group
 
 
 def test_add_group(app):
+        old_groups = app.group.get_group_list()
         app.group.create(Group(name="hfo", header="fghd", footer="sdfdg"))
-
+        new_groups = app.group.get_group_list()
+        assert len(old_groups) + 1 == len(new_groups)
 
 def test_add_empty_group(app):
+        old_groups = app.group.get_group_list()
         app.group.create(Group(name="", header="", footer=""))
-
+        new_groups = app.group.get_group_list()
+        assert len(old_groups) + 1 == len(new_groups)
--- test/test_upd_group.py
+++ test/test_upd_group.py
@@ -4,4 +4,7 @@ from python_training.model.group import Group
 def test_upd_group(app):
         if app.group.count() == 0:
                 app.group.create(Group(name="test1"))
+        old_groups = app.group.get_group_list()
         app.group.update(Group(name="one", header="two", footer="three"))
+        new_groups = app.group.get_group_list()
+        assert len(old_groups) == len(new_groups)
