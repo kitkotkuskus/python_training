4e2dee628eb23b054bedd6f815611bfb1136f611
Dasha <dashaqwa@mail.ru>
2024-08-04 18:43:02 +0300

    Добавлены случайно сгенерированные строки в создание групп и контактов

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,30 +1,41 @@
 # -*- coding: utf-8 -*-
 from python_training.model.contact import Contact
+import pytest
+import random
+import string
 
 
-def test_add_contact(app):
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+def random_number(maxlen):
+    symbols = string.digits + " "*5
+    return "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+def random_mail(prefix):
+    return prefix + '@' + random_string('', 4) + '.' + random_string('', 3)
+
+def random_page(prefix):
+    return prefix + random_string('', 6) + '.' + random_string('', 3)
+
+testdata = [Contact(firstname="", middlename="", lastname="", nickname="", file_place=None, title="", company="",
+                    address="", home="", mobile="", work="", fax="", email="", email2="", email3="", homepage="",
+                    byear="", ayear="")]+[
+    Contact(firstname=random_string("firstname", 10), middlename=random_string("middlename", 10), lastname=random_string("lastname", 10), nickname=random_string("lastname", 10),
+                    file_place="C:\\field_image_muholovka.jpg", title=random_string("title", 10), company=random_string("company", 10),
+                    address=random_string("lastname", 10), home=random_number(11), mobile=random_number(11),
+                    work=random_number(11), fax=random_number(6), email=random_mail("mail"), email2=random_mail("mail"), email3=random_mail("mail"), homepage=random_page("www."),
+                    byear=random_number(4), ayear=random_number(4))
+for i in range(2)
+]
+
+@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+def test_add_contact(app, contact):
     old_contacts = app.contact.get_contact_list()
-    contact = Contact(firstname="Ivan", middlename="Ivanovich", lastname="Ivanov", nickname="IvAnOv",
-                               file_place="C:\\field_image_muholovka.jpg", title="employee", company="bank",
-                               address="Russia, Moscow, Green street 5", home="564585", mobile="89746521278",
-                               work="-", fax="-", email="test@test.ru", email2="-", email3="-", homepage="www.ivanov.ru",
-                               byear="1990", ayear="2000")
     app.contact.create(contact)
     assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
     old_contacts.append(contact)
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
-
-
-
-# def test_add_empty_contact(app):
-#     old_contacts = app.contact.get_contact_list()
-#     contact = Contact(firstname="", middlename="", lastname="", nickname="", file_place=None, title="", company="", address="",
-#                                home="", mobile="", work="", fax="", email="", email2="", email3="", homepage="", byear="", ayear="")
-#     app.contact.create(contact)
-#     new_contacts = app.contact.get_contact_list()
-#     assert len(old_contacts) + 1 == len(new_contacts)
-#     old_contacts.append(contact)
-#     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
-
 
